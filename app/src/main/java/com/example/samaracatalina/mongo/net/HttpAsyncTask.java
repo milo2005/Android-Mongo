53629872796f26080cd1b4f5207aa2ef5cf3308e
Dario Chamorro <dario.chamorro.v@gmail.com>
2015-03-26 10:39:23 -0500

    Clases de Http

--- app/src/main/java/com/example/samaracatalina/mongo/net/HttpAsyncTask.java
+++ app/src/main/java/com/example/samaracatalina/mongo/net/HttpAsyncTask.java
@@ -1,7 +1,80 @@
 package com.example.samaracatalina.mongo.net;
 
+import android.os.AsyncTask;
+
+import java.io.IOException;
+
 /**
  * Created by Mobile 14 on 26/03/2015.
  */
-public class HttpAsyncTask {
+//Entrada: DAtos, Progreso: respuesta, resultado:respuesta
+public class HttpAsyncTask extends AsyncTask<String, String, String> {
+
+    public static final int POST =0;
+    public static final int GET =1;
+
+    public static final int POST_JSON=0;
+    public static final int POST_FORM=1;
+
+    public interface HttpInterface{
+        public void setResponse(String rta);
+    }
+
+
+    int method, posType;
+    String url;
+    HttpConection con;
+
+    HttpInterface httpI;
+
+    public HttpAsyncTask(HttpInterface httpI,String url){
+        this.httpI = httpI;
+        method = GET;
+        this.url = url;
+        con = new HttpConection(url);
+    }
+
+    public HttpAsyncTask(HttpInterface httpI,String url, int postType){
+        this.httpI = httpI;
+        this.url = url;
+        this.posType = postType;
+        method = POST;
+        con = new HttpConection(url);
+    }
+
+
+    @Override
+    protected String doInBackground(String... params) {
+        String rta = null;
+
+        for(int i=0; i<params.length;i++){
+                try {
+                    rta = excuteRequest(params[i]);
+                    publishProgress(rta);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+        }
+        return null;
+    }
+
+    @Override
+    protected void onProgressUpdate(String... values) {
+        httpI.setResponse(values[0]);
+    }
+
+    public String excuteRequest(String data) throws IOException {
+
+        if(method == POST){
+            if(posType == POST_FORM)
+                con.postForm(data);
+            else
+                con.postJson(data);
+            return "OK";
+        }else{
+            String rta = con.getString(data);
+            return rta;
+        }
+
+    }
 }
--- app/src/main/java/com/example/samaracatalina/mongo/net/HttpConection.java
+++ app/src/main/java/com/example/samaracatalina/mongo/net/HttpConection.java
@@ -1,7 +1,104 @@
 package com.example.samaracatalina.mongo.net;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
 /**
  * Created by Mobile 14 on 26/03/2015.
  */
 public class HttpConection {
+
+    HttpURLConnection con;
+    String baseUrl;
+
+    public HttpConection(String baseUrl) {
+        this.baseUrl = baseUrl;
+    }
+
+    //data etiqueta=valor&etiqueta2=valor2
+    public String getString(String data) throws IOException {
+
+        String urlC=baseUrl;
+        if(data!=null)
+            urlC= baseUrl+"?"+data;
+
+        URL url = new URL(urlC);
+
+        con = (HttpURLConnection) url.openConnection();
+        InputStream iS = con.getInputStream();
+        return processStream(iS);
+    }
+
+    public InputStream getStream(String data) throws IOException {
+        String urlC=baseUrl;
+        if(data!=null)
+            urlC= baseUrl+"?"+data;
+
+        URL url = new URL(urlC);
+
+        con = (HttpURLConnection) url.openConnection();
+        InputStream iS = con.getInputStream();
+        return  iS;
+    }
+
+    //form etiqueta=valor&etiqueta2=valor2
+    public void postForm(String form) throws IOException {
+
+        URL url = new URL(baseUrl);
+        con = (HttpURLConnection) url.openConnection();
+
+        con.setRequestMethod("POST");
+        con.setDoOutput(true);
+        OutputStream out = con.getOutputStream();
+
+        OutputStreamWriter writer = new OutputStreamWriter(out);
+
+        writer.write(form);
+        writer.flush();
+        disconnectHttpCon();
+    }
+
+    public void postJson(String json) throws IOException {
+        URL url = new URL(baseUrl);
+        con = (HttpURLConnection) url.openConnection();
+
+        con.setRequestMethod("POST");
+        con.setRequestProperty("Content-Type","application/json");
+        con.setDoOutput(true);
+        OutputStream out = con.getOutputStream();
+
+        OutputStreamWriter writer = new OutputStreamWriter(out);
+
+        writer.write(json);
+        writer.flush();
+        disconnectHttpCon();
+    }
+
+    public String processStream(InputStream iS) throws IOException {
+        InputStreamReader reader = new InputStreamReader(iS);
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        int ch;
+
+        String rta =  null;
+
+        while((ch = reader.read())!=-1)
+            stream.write(ch);
+
+        rta = new String(stream.toByteArray());
+        disconnectHttpCon();
+        return rta;
+    }
+
+    public void disconnectHttpCon(){
+        if(con!=null)
+            con.disconnect();
+    }
 }
